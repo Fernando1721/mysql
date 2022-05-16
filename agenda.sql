4fcecca5e7f485ce5708ff59f124e1a4a574bf68
Fernando1721 <87093495+Fernando1721@users.noreply.github.com>
2022-05-16 16:38:04 -0300

    First Commit Mysql

---
+++ agenda.sql
@@ -0,0 +1,17 @@
+/**
+	Agenda de contatos
+    @author Fernando Miranda
+*/
+
+-- Verificar banco de dados    
+show databases;
+
+-- Criar um novo banco de dados
+create database teste;
+create database agendafernando;
+
+-- Excluir um banco de dados
+drop database teste;
+
+-- Selecionar o banco de dados
+use agendafernando;
